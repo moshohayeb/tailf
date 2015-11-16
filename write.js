0b756476c9fc7d4156988b559b5b3a6984bafac0
Mohammed Alshohayeb <moshohayeb@gmail.com>
2015-11-17 01:55:09 +0300

    removed write.js

--- write.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-
-var i = 0
-var data
-setInterval(function () {
-  data = 'Line ' + i.toString() + '\n'
-  fs.writeFileSync('./Player.txt', data, { flag: 'a+' })
-  i += 1
-}, 50)
